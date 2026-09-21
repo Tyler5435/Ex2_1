307707c42761f8a8b551c705da56cf9951dcde07
Tylerginn <60069935@NA-210-02>
2026-09-21 11:56:56 +0100

    Refactor console color demo, simplify color changes
    Removed background color changes and explanatory comments to streamline the demonstration. Now sets console text color to Red, Yellow, and Green in sequence, displaying a message and pausing for user input after each. Replaced pauses with Console.Read() and Console.ReadKey(). Added unused System.Drawing using directive. Resets console color at the end.

--- Program.cs
+++ Program.cs
@@ -1,24 +1,32 @@
-﻿namespace Ex2_1 // This is a simple program that demonstrates the order of execution of lines of code in C#.
+﻿using System.Drawing;
+
+namespace Ex2_1 // This is a simple program that demonstrates the order of execution of lines of code in C#.
 {
     internal class Program // The Main method is the entry point of the program. It is executed first when the program is run.
     {
         static void Main(string[] args) // The Main method is the entry point of the program. It is executed first when the program is run.
         {
-            Console.WriteLine("This is the first line of the program. It will be executed first.");  //           
-            Console.ReadKey();  // This line of code will pause the program until the user presses a key. It allows the user to see the output of the previous line before continuing.
-            Console.ForegroundColor = ConsoleColor.Red; // This line of code changes the colour of the text in the console to red. It will affect all subsequent lines of code that write to the console.
-            Console.WriteLine("But then the colour changes to Red."); // This line of code will be executed after the previous line, and it will be displayed in red text.
-            Console.ReadKey();// This line of code will pause the program until the user presses a key. It allows the user to see the output of the previous line before continuing.
-            Console.BackgroundColor = ConsoleColor.White; // This line of code changes the background colour of the console to white. It will affect all subsequent lines of code that write to the console.
-            Console.WriteLine("And the background goes white"); // This line of code will be executed after the previous line, and it will be displayed with a white background.
-            Console.ReadKey(); //   This line of code will pause the program until the user presses a key. It allows the user to see the output of the previous line before continuing.
-            Console.ResetColor(); // This line of code resets the console colours to their default values. It will affect all subsequent lines of code that write to the console.
-            Console.WriteLine("But it's okay. We can reset it"); // This line of code will be executed after the previous line, and it will be displayed with the default console colours.
-            Console.ReadKey(); // This line of code will pause the program until the user presses a key. It allows the user to see the output of the previous line before continuing.
-            Console.ForegroundColor = ConsoleColor.Yellow; //   This line of code changes the colour of the text in the console to yellow. It will affect all subsequent lines of code that write to the console.
-            Console.WriteLine("The order of lines of code is important"); // This line of code will be executed after the previous line, and it will be displayed in yellow text.
-            Console.WriteLine("We start at the top and work down"); // This line of code will be executed after the previous line, and it will be displayed in yellow text.
-            Console.ReadKey(); // This line of code will pause the program until the user presses a key. It allows the user to see the output of the previous line before continuing.
-        }
+            // The following line of code changes the color of the console text to red.
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("The Color is Red");
+            Console.Read();
+            
+            // The following line of code changes the color of the console text to Amber.
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("The Color is Amber");
+            Console.ReadKey();
+
+            // The following line of code adds a blank line to the console output for better readability.g
+            Console.WriteLine("");
+
+            // The following line of code changes the color of the console text to Green.
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("The Color is Green");
+            Console.ReadKey();
+
+            // The following line of code resets the color of the console text to the default color.
+            Console.ResetColor();
+
+        } // The Main method ends here, and the program will exit after the last line of code is executed.
     }
 }
